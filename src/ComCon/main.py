b4934d97f128db4d3326e7587594a834416e6e13
dmknght <dmknght@protonmail.com>
2019-07-23 06:26:55 +0700

    Add exception for arguments len <testing only>

--- src/ComCon/main.py
+++ src/ComCon/main.py
@@ -4,10 +4,11 @@
 import sys
 
 try:
-	print(sys.argv)
 	_, lhost, lport, key = sys.argv
 	lport, key = int(lport), int(key)
 	import server
 	c2c = server.Server(lhost, lport, key)
+except ValueError:
+	print("%s <lhost> <lport> <xor key (int)>" %(sys.argv[0]))
 except Exception as error:
 	print("[x] Error: %s" %(error))
