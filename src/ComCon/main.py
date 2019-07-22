94fe4448ca75e3c69cd91f36dbf1d6d12460068e
dmknght <dmknght@protonmail.com>
2019-07-22 20:19:12 +0700

    Remove wrong import

--- src/ComCon/main.py
+++ src/ComCon/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import interface
+import sys
 
-lhost = "127.0.0.1"
-lport = 8888
-key = 16
-import server
-c2c = server.Server(lhost, lport, 16)
+try:
+	print(sys.argv)
+	_, lhost, lport, key = sys.argv
+	lport, key = int(lport), int(key)
+	import server
+	c2c = server.Server(lhost, lport, key)
+except Exception as error:
+	print("[x] Error: %s" %(error))
